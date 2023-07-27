c0d1fb5e7433000be1cb21f646c2f5c8a224c2e5
John <ashton.basile@gmail.com>
2023-07-27 15:49:28 +0100

    I updated my app.py file to register my blueprints

--- app.py
+++ app.py
@@ -14,3 +14,18 @@ app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://postgres:{PASSWORD}@local
 app.config["SQLALCHEMY_ECHO"] = True
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+from controllers.booking_controller import bookings_blueprint
+from controllers.treatment_controller import treatments_blueprint
+from controllers.customer_controller import customers_blueprint
+
+app.register_blueprint(bookings_blueprint)
+app.register_blueprint(treatments_blueprint)
+app.register_blueprint(customers_blueprint)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
