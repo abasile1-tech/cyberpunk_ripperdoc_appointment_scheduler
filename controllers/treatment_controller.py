11e7b81ddcdb1a4ed7084bc4567d036efbfd4107
John <ashton.basile@gmail.com>
2023-07-28 14:13:22 +0100

    I cleaned up some of the formatting of the pages

--- controllers/treatment_controller.py
+++ controllers/treatment_controller.py
@@ -10,11 +10,37 @@ treatments_blueprint = Blueprint("treatments", __name__)
 
 @treatments_blueprint.route("/treatments")
 def treatments():
-    treatments = Treatment.query.all()
-    return render_template("treatments/index.html", treatments = treatments)
+	treatments = Treatment.query.all()
+	return render_template("treatments/index.html", treatments = treatments)
 
 @treatments_blueprint.route("/treatments/<id>")
 def show(id):
-    treatment = Treatment.query.get(id)
-    customers = Customer.query.join(Booking).filter(Booking.treatment_id == id)
-    return render_template("treatments/show.html", treatment=treatment, customers=customers)
+	treatment = Treatment.query.get(id)
+	customers = Customer.query.join(Booking).filter(Booking.treatment_id == id)
+	return render_template("treatments/show.html", treatment=treatment, customers=customers)
+
+# NEW
+# GET '/treatments/new'
+@treatments_blueprint.route("/treatments/new")
+def new_booking():
+	return render_template("treatments/new.html")
+
+# CREATE
+# POST '/treatments'
+@treatments_blueprint.route("/treatments",  methods=['POST'])
+def create_treatment():
+	name = request.form['name']
+	duration = request.form['duration']
+	treatment = Treatment(name=name, duration=duration)
+	db.session.add(treatment)
+	db.session.commit()
+	return redirect('/treatments')
+
+
+# DELETE
+# DELETE '/treatments/<id>/delete'
+@treatments_blueprint.route("/treatments/<id>/delete", methods=['POST'])
+def delete_treatment(id):
+	Treatment.query.filter_by(id = id).delete()
+	db.session.commit()
+	return redirect('/treatments')
