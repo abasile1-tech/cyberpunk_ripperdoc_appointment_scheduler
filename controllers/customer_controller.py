7223eb8ec366dca7931d02e919d31d1e6777bc5e
John <ashton.basile@gmail.com>
2023-07-28 11:58:54 +0100

    New customers can now be added

--- controllers/customer_controller.py
+++ controllers/customer_controller.py
@@ -3,17 +3,43 @@ from flask import Blueprint
 from models.Booking import Booking
 from models.Customer import Customer
 from models.Treatment import Treatment
+from app import db
 
 customers_blueprint = Blueprint("customers", __name__)
 
 @customers_blueprint.route("/customers")
 def customers():
-    customers = Customer.query.all()
-    return render_template("customers/index.html", customers = customers)
+	customers = Customer.query.all()
+	return render_template("customers/index.html", customers = customers)
 
 @customers_blueprint.route("/customers/<id>")
 def show(id):
-    customer = Customer.query.get(id)
-    treatments = Treatment.query.join(Booking).filter(Booking.customer_id == id)
-    bookings = Booking.query.filter(Booking.customer_id == id)
-    return render_template("customers/show.html", customer=customer, treatments=treatments, bookings=bookings)
+	customer = Customer.query.get(id)
+	treatments = Treatment.query.join(Booking).filter(Booking.customer_id == id)
+	bookings = Booking.query.filter(Booking.customer_id == id)
+	return render_template("customers/show.html", customer=customer, treatments=treatments, bookings=bookings)
+
+# NEW
+# GET '/customers/new'
+@customers_blueprint.route("/customers/new")
+def new_booking():
+	return render_template("customers/new.html")
+
+# CREATE
+# POST '/customers'
+@customers_blueprint.route("/customers",  methods=['POST'])
+def create_customer():
+	name = request.form['name']
+	customer = Customer(name=name)
+	db.session.add(customer)
+	db.session.commit()
+	return redirect('/customers')
+
+
+# DELETE
+# DELETE '/customers/<id>/delete'
+@customers_blueprint.route("/customers/<id>/delete", methods=['POST'])
+def delete_customer(id):
+	Customer.query.filter_by(id = id).delete()
+	db.session.commit()
+	return redirect('/customers')
